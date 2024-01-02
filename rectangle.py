710356074faef6db1be22988bf4b89ab290f8b34
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-01-02 23:57:51 +0530

    even better writer

--- rectangle.py
+++ rectangle.py
@@ -18,11 +18,6 @@ def interpret(program, input):
     i, j = input
     return i >= T and i <= D and j >= L and j <= R
 
-assert interpret(rectangle, grass1) == inside
-assert interpret(rectangle, grass2) == inside
-assert interpret(rectangle, shroom1) == outside
-assert interpret(rectangle, shroom2) == outside
-
 
 # specification is a list of input output examples
 spec = [(grass1, inside), (grass2, inside), (shroom1, outside), (shroom2, outside)]
@@ -53,12 +48,6 @@ def get_synthesizer(writer, checker, budget):
         return None
     return synthesizer
 
-synthesizer = get_synthesizer(random_writer, is_correct, 1000)
-spec1 = [((0,4), outside), ((4,1), outside), ((1,1), inside), ((3,3), inside)]
-res = synthesizer(spec1)
-if res is not None:
-    print(res)
-
 # use grass coords to guess better
 def better_writer(spec):
     inside_coords = [coord for coord,bool in spec if bool]
@@ -70,7 +59,31 @@ def better_writer(spec):
     L, R = random.choice(col_coords), random.choice(col_coords)
     return [T, D, L, R]
 
-synthesizer2 = get_synthesizer(better_writer, is_correct, 1000)
-res = synthesizer2(spec1)
-if res is not None:
-    print(res)
+
+def even_better_writer(spec):
+    inside_coords = [coord for coord,bool in spec if bool]
+    if inside_coords == []:
+        return random_writer(spec)
+    row_coords = [coord[0] for coord in inside_coords]
+    col_coords = [coord[1] for coord in inside_coords]
+    T, D = min(row_coords), max(row_coords)
+    L, R = min(col_coords), max(col_coords)
+    return [T, D, L, R]
+
+if __name__ == '__main__':
+    spec1 = [((0,4), outside), ((4,1), outside), ((1,1), inside), ((3,3), inside)]
+    
+    synthesizer = get_synthesizer(random_writer, is_correct, 1000)
+    res = synthesizer(spec1)
+    if res is not None:
+        print(res)
+
+    synthesizer = get_synthesizer(better_writer, is_correct, 1000)
+    res = synthesizer(spec1)
+    if res is not None:
+        print(res)
+
+    synthesizer = get_synthesizer(better_writer2, is_correct, 1000) 
+    res = synthesizer(spec1)
+    if res is not None:
+        print(res)
